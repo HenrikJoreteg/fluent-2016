156fc2f3d47d6e233311baac9c575d1ad21a40f5
Henrik Joreteg <hjoreteg@gmail.com>
2016-03-07 10:49:46 -0800

    Mon Mar  7 10:49:46 PST 2016

--- src/root.js
+++ src/root.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
-import './styles/main.css'
+import './styles/main.styl'
 
 render(<App name='asdfa'/>, document.body.firstChild)
