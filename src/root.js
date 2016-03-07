97f3d0f7029d4eedfeaf097ac898c692e20af128
Henrik Joreteg <hjoreteg@gmail.com>
2016-03-07 12:03:36 -0800

    Mon Mar  7 12:03:36 PST 2016

--- src/root.js
+++ src/root.js
@@ -2,5 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
 import './styles/main.styl'
+import createStore from './store'
+
+const store = window.store = createStore()
 
 render(<App url={window.location.pathname}/>, document.body.firstChild)
