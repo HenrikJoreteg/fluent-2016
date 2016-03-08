76e54d35e6e9ba707f776ff43d177032b3ab460c
Henrik Joreteg <hjoreteg@gmail.com>
2016-03-08 14:49:11 -0800

    Tue Mar  8 14:49:11 PST 2016

--- src/actions.js
+++ src/actions.js
@@ -2,8 +2,8 @@ import qs from 'query-string'
 import fetchHelper from './helpers/fetch-helper'
 
 export const UPDATE_URL = 'UPDATE_URL'
-export const updateUrl = (url, options = {replace: false}) => {
-  if (options.replace) {
+export const updateUrl = (url, options) => {
+  if (options && options.replace) {
     window.history.replaceState({}, null, url)
   }
   return { type: UPDATE_URL, payload: url, replace: options.replace }
