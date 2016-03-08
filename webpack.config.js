9ae5cb8962485b82bffa454018609cdc4f34538c
Henrik Joreteg <hjoreteg@gmail.com>
2016-03-08 15:08:09 -0800

    Tue Mar  8 15:08:09 PST 2016

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,11 @@ var getConfig = require('hjs-webpack')
 module.exports = getConfig({
   in: 'src/root.js',
   out: 'public',
-  clearBeforeBuild: '!(images|static)'
+  clearBeforeBuild: '!(images|static)',
+  html: function (context) {
+    return {
+      'index.html': context.defaultTemplate(),
+      '200.html': context.defaultTemplate()
+    }
+  }
 })
